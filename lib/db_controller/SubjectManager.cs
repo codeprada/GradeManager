3b8f66dd0b122175a65e2a6ec418249a0648aa6d
Chavez Watkins <lilrwatty@gmail.com>
2013-07-08 22:42:23 -0400

    File Version Fix

--- lib/db_controller/SubjectManager.cs
+++ lib/db_controller/SubjectManager.cs
@@ -14,6 +14,21 @@ namespace Grade_Manager_DB_Controller
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static implicit operator Subject(SQLiteDataReader reader)
+        {
+            Subject subject = new Subject();
+
+            subject.Id = Convert.ToInt32(reader["subject_id"]);
+            subject.Name = Convert.ToString(reader["subject_name"]);
+
+            return subject;
+        }
     }
 
     public class SubjectManager : BaseManager
@@ -26,13 +41,27 @@ namespace Grade_Manager_DB_Controller
 
         public List<Subject> GetAllSubjects()
         {
+            List<Subject> subjects = new List<Subject>();
+
             using (var connection = new SQLiteConnection(ConnectionString))
             {
                 using (SQLiteCommand command = new SQLiteCommand(GradeManager_SQLite_DB_Controller.DBQ_GET_ALL_SUBJECTS, connection))
                 {
+                    command.CommandType = CommandType.Text;
 
+                    connection.Open();
+
+                    SQLiteDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        //implicit operator does the conversion
+                        subjects.Add(reader);
+                    }
                 }
             }
+
+            return subjects;
         }
         
     }
